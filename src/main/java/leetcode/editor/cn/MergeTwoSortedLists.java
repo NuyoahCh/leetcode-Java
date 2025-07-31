0023439cd962546a5e8988ed60a2caaff1f029fd
NuyoahCh <springoflotus@gmail.com>
2025-07-31 16:33:51 +0800

    first commit

---
+++ src/main/java/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ * @lcpr version=30008
+ *
+ * [21] 合并两个有序链表
+ */
+
+package leetcode.editor.cn;
+
+import java.util.*;
+
+import leetcode.editor.common.*;
+
+public class MergeTwoSortedLists {
+
+    // @lc code=start
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        }
+    }
+    // @lc code=end
+
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+        // put your test code here
+
+}
+
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+
